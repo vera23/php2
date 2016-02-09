4caf06e5c49ab54faf3c49c87b538075fb2a9939
unknown <veramir10@gmail.com>
2016-02-09 18:09:54 +0300

    author is in /Index/Default.html

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__ . '/autoload.php';
 
-$view = new \App\View();
+/*$view = new \App\View();
 $view->title = 'Why not?';
 $view->users = \App\Models\User::findAll();
-$view->display(__DIR__ . '/App/templates/index.php');
+$view->display(__DIR__ . '/App/templates/index.php');*/
 
 
 
-
+$articles = \App\Models\News::findAll();
+include __DIR__ . '/App/Views/News/Index/Default.html';
