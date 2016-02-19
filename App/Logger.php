0ac7d4a193f75a41e59a4f6eee1546540da2d805
unknown <veramir10@gmail.com>
2016-02-19 09:39:45 +0300

    commit

---
+++ App/Logger.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+
+class Logger extends \Exception
+{
+    public function log()
+    {
+    catch{
+
+    }
+    }
+
+
+}
--- index.php
+++ index.php
@@ -7,8 +7,12 @@ try {
     $app->run();
 } catch (Exception $e) {
     if ($e instanceof PDOException) {
-
+        include __DIR__ . '\App\templates\error.php';
     }
+    if($e instanceof \App\Exceptions\E404Exception) {
+        include __DIR__ . '\App\templates\e404.php';
+    }
+    throw $e;
 }
 
 
