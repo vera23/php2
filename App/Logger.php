5532ec9b66e8bb65c0b66d417d00a3c4a8912d88
unknown <veramir10@gmail.com>
2016-02-29 08:55:01 +0300

    modified Logger class

--- App/Logger.php
+++ App/Logger.php
@@ -2,13 +2,29 @@
 
 namespace App;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
 
-class Logger
+class Logger implements LoggerInterface
 {
-    public static function write(\Exception $e) {
-        $fp = fopen('error_logs/err_log.txt', 'a');
-        fwrite($fp, date('y-m-d G:i:s') . ': Ошибка класса ' . get_class($e) . ' '. $e->getMessage() . "\n" );
-        fclose($fp);
+    use LoggerTrait;
+
+    public function log($level, $message, array $context = array())
+    {
+        $e = $context['exception'];
+        if (isset($e) && $e instanceof \Exception) {
+            $fp = fopen('error_logs/err_log.txt', 'a');
+            fwrite($fp,
+                date('y-m-d G:i:s') . ': Ошибка класса ' . get_class($e) . '( ' . $level . ' )' . $message .
+                'in file: ' . $e->getFile() . 'on line ' . $e->getLine() . "\n");
+            fclose($fp);
+        }
     }
 
+    /* public static function write(\Exception $e)
+     {
+         $fp = fopen('error_logs/err_log.txt', 'a');
+         fwrite($fp, date('y-m-d G:i:s') . ': Ошибка класса ' . get_class($e) . ' ' . $e->getMessage() . "\n");
+         fclose($fp);
+     }*/
 }
